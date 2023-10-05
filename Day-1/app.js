1f812d98a097013a53a9669a388d6de8d14290d6
SudheerShrinivas <sudheershrinivas@gmail.com>
2023-10-05 21:37:48 +0530

    Code

---
+++ Day-1/app.js
@@ -0,0 +1,47 @@
+console.log("Hi JavaScript")
+// & JavaScript featurs
+// ^
+// !
+// ?
+// ~
+// *
+
+// &.1 Dynamic
+
+//! Number
+// var a =20
+// console.log(a)
+// ! String
+// var a="JavaScript"
+// console.log(a)
+// ! object
+// var a={}
+// console.log(a)
+// ! function
+// var a=()=>{}
+// console.log(a)
+// !Array
+// var a=[]
+// console.log(a)
+
+
+// & Interprted
+// console.log("one")
+// console.log("Two")
+// console.log("Three")
+// console.log("Four")
+// console.log("Five")
+// console.log("Six")
+
+
+// & Synchronus
+// console.log("one")
+// console.log("Two")
+// console.log("Three")
+// console.log("Four")
+// console.log("Five")
+// console.log("Six")
+
+
+// & Weekly typed and Loosly Typed
+console.log("Hi");console.log("Hello")
